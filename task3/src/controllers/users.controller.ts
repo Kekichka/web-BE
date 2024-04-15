17a3346d9a80438b7689e5720b62377b054dd26f
kekichka <alonadaniluk695@gmail.com>
2024-04-15 22:34:09 +0300

    done

--- task3/src/controllers/users.controller.ts
+++ task3/src/controllers/users.controller.ts
@@ -4,12 +4,12 @@ import { LoginDto, UserDto, AdminDto, DriverDto } from '../models';
 import { UserAlreadyExists, UserNotFound } from '../shared'; 
 import { UserAuthorizationMiddleware } from 'src/midellware/userAuthorization.middleware';
 
-@Controller('users')
+@Controller ('users')
 export class UsersController {
   constructor(private readonly userService: UserService) { }
 
-  @Post()
-  async createUser(@Body() body: UserDto) {
+  @Post ()
+  async createUser (@Body() body: UserDto) {
     try {
       const result = await this.userService.createUser(body);
       return result;
@@ -21,7 +21,7 @@ export class UsersController {
     }
   }
 
-  @Post('login')
+  @Post ('login')
   async login(@Body() body: LoginDto) {
     try {
       const result = await this.userService.login(body);
