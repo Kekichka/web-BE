b96e8fd89fb2fca941a89287b80c9b519549db2c
kekichka <alonadaniluk695@gmail.com>
2024-05-12 08:44:30 +0300

    progress

--- task6/src/models/links.dto.ts
+++ task6/src/models/links.dto.ts
@@ -29,3 +29,4 @@ export class RandomPartDto {
     height: number;
   };
 }
+
--- task6/src/service/parts.service.ts
+++ task6/src/service/parts.service.ts
@@ -2,20 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PartsService {
-  private readonly parts: any[] = [
-    {
-      imageUrl: 'https://example.com/image1.jpg',
-      box: { x: 10, y: 20, width: 100, height: 200 }
-    },
-    {
-      imageUrl: 'https://example.com/image2.jpg',
-      box: { x: 30, y: 40, width: 150, height: 250 }
-    },
-  ];
-
   async getRandomPart(): Promise<any> {
-    
-    const randomIndex = Math.floor(Math.random() * this.parts.length);
-    return this.parts[randomIndex];
+    const randomPart = {
+      imageUrl: 'https://example.com/random-image.jpg',
+      box: {
+        x: this.generateRandomNumber(0, 100), 
+        y: this.generateRandomNumber(0, 100), 
+        width: this.generateRandomNumber(50, 200), 
+        height: this.generateRandomNumber(50, 200), 
+      }
+    };
+    return randomPart;
   }
+
+  private generateRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  
 }
